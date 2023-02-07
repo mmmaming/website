9d0f6126b5d52a09030744364fd8aaec81a22463
ming.ma <ming.ma@rea-group.coma>
2023-02-07 19:25:25 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+// @ts-ignore
+// import logo from '%PUBLIC_URL%/logo192.png';
 import './App.css';
+const logo = process.env.PUBLIC_URL+'/logo192.png';
 
 function App() {
+  console.log(process.env)
+  useEffect(() => {
+    axios.get("/x/asd");
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src="/images/llo.png" className="App-logo" alt="logo" />
         <p>
+          EEEEE
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
